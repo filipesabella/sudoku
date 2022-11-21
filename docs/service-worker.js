a867d54349c9c2e62f68ccaf9d4eb82aabd8c6d4
Filipe Sabella <me@filipesabella.com>
2022-11-21 16:13:15 -0300

    Release

---
+++ docs/service-worker.js
@@ -0,0 +1,2 @@
+(()=>{let a=[],n="";a=["index.html","android-icon-192x192.a9beedbe.png","favicon-32x32.6c82a014.png","favicon-96x96.e83e89ba.png","index.d106764b.css","index.720e2f31.js"],n="c88faea0",addEventListener("install",(e=>e.waitUntil(async function(){const e=await caches.open(n);await e.addAll(a)}()))),addEventListener("activate",(a=>a.waitUntil(async function(){const a=await caches.keys();await Promise.all(a.map((a=>a!==n&&caches.delete(a))))}())))})();
+//# sourceMappingURL=service-worker.js.map
